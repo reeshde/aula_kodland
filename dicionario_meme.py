6d8eeaeb8611ab2a090b4b23df9a72580751b9f9
reeshde <pedrohenriquedasilva@ienh.com.br>
2026-06-02 20:30:58 -0300

    Create dicionario_meme.py

---
+++ dicionario_meme.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "CRINGE": "Algo vergonhoso ou constrangedor",
+            "STALKEAR": "Investigar a vida de alguém online",
+            "VDD": "abreviação da palavra verdade",
+            "BISCOITAR": "postar algo apenas para chamar a atenção",
+            "HATER": "pessoa que está constantemente criticando os outros",
+            "VLW": "abreviação da palavra valeu",
+            }
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("tente novamente")
